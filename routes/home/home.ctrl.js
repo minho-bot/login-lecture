d6cec108ff3538c8690c32f2d319c3b9e393a506
younghee <dnslskqk123@naver.com>
2023-02-16 20:02:20 +0900

    깃 저장소 초기화

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const home = (req, res)=>{
+    res.render("home/index.ejs");
+};
+
+const login = (req, res)=>{
+    res.render("home/login.ejs");
+};
+
+module.exports = {
+    home,
+    login,
+};
